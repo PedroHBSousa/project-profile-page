4fbc1de96e575fdf53448a5a1681c6cc33fb5ad5
Pedro Sousa <pedro795027@gmail.com>
2024-12-04 15:35:26 -0300

    feat: Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+</script>
+
+<template>
+  <div>
+    <Header />
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+    <Footer />
+  </div>
+</template>
